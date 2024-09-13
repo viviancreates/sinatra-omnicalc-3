7f7f83af74bb9d7e0694ab48d269947c7c4030f0
viviancreates <168599473+viviancreates@users.noreply.github.com>
2024-09-13 03:13:06 +0000

    Break

--- app.rb
+++ app.rb
@@ -13,8 +13,11 @@ get("/umbrella") do
 end
 
 get("/process_umbrella") do
-
   @user_location = params.fetch("user_loc")
+
+  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=Merchandise%20Mart%20Chicago&key=AIzaSyDKz4Y3bvrTsWpPRNn9ab55OkmcwZxLOHI"
+  
+  @raw_response = HTTP.get(gmaps_url).to_s
 
   erb(:umbrella_results)
 end
