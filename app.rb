b0c95eabe61db664847cfeda43a922aa6274cbbf
viviancreates <168599473+viviancreates@users.noreply.github.com>
2024-09-13 03:47:45 +0000

    Switch get to post

--- app.rb
+++ app.rb
@@ -15,19 +15,18 @@ get("/umbrella") do
   erb(:umbrella_form)
 end
 
-get("/process_umbrella") do
+post("/process_umbrella") do
   @user_location = params.fetch("user_loc")
 
   url_encoded_string = @user_location.gsub("", "+")
 
-  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{url_encoded_string}&key=GMAPS_KEY"
+  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{url_encoded_string}&key=" + ENV.fetch("GMAPS_KEY")
   
   @raw_response = HTTP.get(gmaps_url).to_s
 
   @parsed_response = JSON.parse(@raw_response)
 
-  @loc_hash = @parsed_response.dig("results", 0, "geometry, "location")
-
+  @loc_hash = @parsed_response.dig("results", 0, "geometry", "location")
   @latitude = @loc_hash.fetch("lat")
   @longitude = @loc_hash.fetch("lng")
 
